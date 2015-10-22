78fcb71e3428c1cc91e5261abe2078d085248fae
rafik347 <259048@stud.umk.pl>
2015-10-22 20:26:55 +0200

    Tlo, kamera, gwiazdki (komentarze)

--- Projekt/Assets/Code/SortParticleSystem.cs
+++ Projekt/Assets/Code/SortParticleSystem.cs
@@ -5,6 +5,8 @@ public class SortParticleSystem : MonoBehaviour
 {
     public string LayerName = "Particles";
 
+    // Domyślnie ParticleSystem nie ma opcji ustawiania warstwy, 
+    // na której ma być wyświetlany.
     public void Start()
     {
         particleSystem.renderer.sortingLayerName = LayerName;
